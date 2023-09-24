36f21c64507fb5ed03417536bf496e375ed3ed3a
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-09-24 16:24:00 -0700

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "sort.h"
+#include <stdbool.h>
 /**
  * swap_ints - a function that swaps integers
  * @a: the first integer
@@ -23,20 +24,20 @@ void swap_ints(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	int bubble = 0;
+	bool bubble = false;
 
 	if (array == NULL || size < 2)
 		return;
-	while (bubble == 0)
+	while (bubble == false)
 	{
-		bubble = 1;
+		bubble = true;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubble = 0;
+				bubble = false;
 			}
 
 		}
