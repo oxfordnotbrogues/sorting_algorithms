3da8155b0a7447abf0ed0c2bc9664443fb44782e
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-09-25 04:34:44 -0700

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,32 +1,39 @@
 #include "sort.h"
-
 /**
- * selection_sort - A function that use selection sort algorithm.
- * @array: An array to sort.
- * @size: The size of the array.
- * Return: Nothing.
+ * swap_ints - swap two integers in an array
+ * @a: the first integer to swap
+ * @b: the second integer to swap
+ */
+void swap_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * selection_sort - sort an array of integers in ascending order
+ * using the selection sort algorithm
+ * @array: an array of integers
+ * @size: the size of the array
+ * Description: prints the array after each swap
  */
 void selection_sort(int *array, size_t size)
 {
-	int temp = 0;
-	size_t i = 0, j = 0, sop = 0;
+	int *min;
+	size_t i, j;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
-
-	for (; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		sop = i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+		if ((array + i) != min)
 		{
-			if (array[j] < array[sop])
-				sop = j;
-		}
-		if (sop != i)
-		{
-			temp = array[i];
-			array[i] = array[sop];
-			array[sop] = temp;
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
